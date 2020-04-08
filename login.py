56e3621381bfa61f8f2aa2b0b4411836aacdcbd0
lrr <921090271@qq.com>
2020-04-09 00:22:30 +0800

    完成原创微博有图内容的爬取，尚未完成获取微博全文与图片

--- login.py
+++ login.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 
+
 class WeiboLogin():
     def __init__(self, username="0331_ee6ebc@163.com", password="cua633476"):
         self.url = "https://passport.weibo.cn/signin/login"
@@ -14,14 +15,15 @@ class WeiboLogin():
         self.option.add_argument("--headless")
         self.option.add_argument("--disable-gpu")
         self.browser = webdriver.Firefox()
-        self.browser.set_window_size(1050,840)
-        self.wait = WebDriverWait(self.browser,20)
+        self.browser.set_window_size(1050, 840)
+        self.wait = WebDriverWait(self.browser, 20)
         self.username = username
         self.password = password
         self.uids = ['1743951792']  # 美国大使馆的uid
-        self.ids = ['usembassy'] # 美国驻华大使馆的id
+        self.ids = ['usembassy']  # 美国驻华大使馆的id
 
         self.id_url = 'http://weibo.cn/'
+        self.page_url = 'http://weibo.cn/{}?page={}'
         print("初始化完成")
 
     def open(self):
@@ -30,12 +32,13 @@ class WeiboLogin():
         :return:
         '''
         self.browser.get(self.url)
-        username = self.wait.until(EC.presence_of_element_located((By.ID,'loginName')))
-        password = self.wait.until(EC.presence_of_element_located((By.ID,'loginPassword')))
-        submit = self.wait.until(EC.element_to_be_clickable((By.ID,'loginAction')))
+        username = self.wait.until(EC.presence_of_element_located((By.ID, 'loginName')))
+        password = self.wait.until(EC.presence_of_element_located((By.ID, 'loginPassword')))
+        submit = self.wait.until(EC.element_to_be_clickable((By.ID, 'loginAction')))
         username.send_keys(self.username)
         password.send_keys(self.password)
         submit.click()
+
     def getCookies(self):
         '''
         破解入口
@@ -44,21 +47,21 @@ class WeiboLogin():
         self.open()
 
         try:
-            WebDriverWait(self.browser,30).until(
-                EC.presence_of_element_located((By.CLASS_NAME,"geetest_radar_tip"))
+            WebDriverWait(self.browser, 30).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "geetest_radar_tip"))
             )
             print('当前页面的url', self.browser.current_url)
             f_test = self.browser.find_element_by_id('message-p')
             print("f_test", f_test.text)
             check_input = self.browser.find_element_by_class_name('geetest_radar_tip')
-            check_input.click() # 点击
+            check_input.click()  # 点击
             print("按钮点击验证完成")
         except Exception as e:
             print("验证失败")
             print(e)
 
         try:
-            WebDriverWait(self.browser,30).until(
+            WebDriverWait(self.browser, 30).until(
                 EC.title_is("微博")
             )
 
@@ -74,36 +77,82 @@ class WeiboLogin():
 
     def getUserInfoAndWeibo(self):
         for id in self.ids:
-            id_url = self.id_url+id
+            id_url = self.id_url + id
             print(id_url)
             self.browser.get(id_url)
-            try:
-                WebDriverWait(self.browser,3).until(
-                    EC.title_is("美国驻华大使馆的微博")
-                )
+            # try:
+            WebDriverWait(self.browser, 3).until(
+                EC.title_is("美国驻华大使馆的微博")
+            )
+            # 使用BeautifulSoup解析网页的HTML
+            soup = BeautifulSoup(self.browser.page_source, 'lxml')
+            # 爬取最大页码数目
+            pageSize = soup.find('div', attrs={'id': 'pagelist'})
+            pageSize = pageSize.find('div').getText()
+            pageSize = (pageSize.split('/')[1]).split('页')[0]
+
+            # 爬取微博数量
+            divMessage = soup.find('div', attrs={'class': 'tip2'})
+            weiBoCount = divMessage.find('span').getText()
+            weiBoCount = (weiBoCount.split('[')[1]).replace(']', '')
+            # 爬取微博数量
+            divMessage = soup.find('div', attrs={'class': 'tip2'})
+            weiBoCount = divMessage.find('span').getText()
+            weiBoCount = (weiBoCount.split('[')[1]).replace(']', '')
+            # 爬取关注数量和粉丝数量
+            a = divMessage.find_all('a')[:2]
+            guanZhuCount = (a[0].getText().split('[')[1]).replace(']', '')
+            fenSiCount = (a[1].getText().split('[')[1]).replace(']', '')
+            print("最大页码 {} 微博数量 {} 粉丝数量 {} 关注数量 {}".format(pageSize, weiBoCount, fenSiCount, guanZhuCount))
+
+            for page_index in range(1, int(pageSize)):
+                page_url = self.page_url.format(id, page_index)
+                print('page_url', page_url)
+                self.browser.get(page_url)
+                time.sleep(1)
                 # 使用BeautifulSoup解析网页的HTML
-                soup = BeautifulSoup(self.browser.page_source,'lxml')
-                # 爬取最大页码数目
-                pageSize = soup.find('div', attrs={'id': 'pagelist'})
-                pageSize = pageSize.find('div').getText()
-                pageSize = (pageSize.split('/')[1]).split('页')[0]
+                soup = BeautifulSoup(self.browser.page_source, 'lxml')
+                body = soup.find('body')
+                # 具体的数值可通过页面查看
+                div_all = body.find_all_next('div', attrs={'class': 'c'})[1:-2]
+                for divs in div_all:
+                    # yuanChuang : 0表示转发，1表示原创
+                    yuanChuang = '1'  # 初始值为原创，当非原创时，更改此值
+                    div = divs.find_all("div")
 
-                # 爬取微博数量
-                divMessage = soup.find('div', attrs={'class': 'tip2'})
-                weiBoCount = divMessage.find('span').getText()
-                weiBoCount = (weiBoCount.split('[')[1]).replace(']', '')
-                # 爬取微博数量
-                divMessage = soup.find('div', attrs={'class': 'tip2'})
-                weiBoCount = divMessage.find('span').getText()
-                weiBoCount = (weiBoCount.split('[')[1]).replace(']', '')
-                # 爬取关注数量和粉丝数量
-                a = divMessage.find_all('a')[:2]
-                guanZhuCount = (a[0].getText().split('[')[1]).replace(']', '')
-                fenSiCount = (a[1].getText().split('[')[1]).replace(']', '')
-                print("最大页码 {} 微博数量 {} 粉丝数量 {} 关注数量 {}".format(pageSize,weiBoCount, fenSiCount, guanZhuCount))
+                    if len(div) == 1:  # 原创无图
+                        pass
+                    elif len(div) == 2:  # 原创有图
+                        content = div[0].find('span', attrs={'class': 'ctt'}).getText()
+                        aa = div[1].find_all('a')
+                        for a in aa:
+                            text = a.getText()
+                            if "赞" in text:
+                                dianZhan = (text.split('[')[1]).replace(']', '')
+                            elif ('转发' in text):
+                                zhuanFa = (text.split('[')[1]).replace(']', '')
+                            elif ('评论' in text):
+                                pinLun = (text.split('[')[1]).replace(']', '')
+                        # 获取来源和时间
+                        span = divs.find('span', attrs={'class': "ct"}).getText()
+                        print('原创有图 span',span)
+                        span_split = span.split("来自")
+                        print(span.split("来自"))
 
-            except Exception as e:
-                print("抓取id为：{} 的信息失败".format(id))
+                        faBuTime = str(span_split[0])
+                        # laiYuan = span.split("来自")[1]
+                        laiYuan = span_split[1] if len(span_split) == 2 else "无"
+
+                    elif len(div) == 3:  # 转发
+                        pass
+
+                print("微博内容 {} \n 赞 {} 转发 {} 评论 {}".format(content, dianZhan, zhuanFa, pinLun))
+
+        # except Exception as e:
+        #     print("抓取id为：{} 的信息失败".format(id))
+        #     self.browser.quit()
+        #     print(e)
+
 
 if __name__ == '__main__':
     # try:
